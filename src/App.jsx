933fa69685cdde44a94a9cf5c1918d278b66dc9a
Vera Ferreira <verammferreira@gmail.com>
2023-08-14 13:13:22 +0100

    add HEader component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./index.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
   return (
     <section className="app">
       <Header />
+      <Main />
       <Footer />
     </section>
   );
